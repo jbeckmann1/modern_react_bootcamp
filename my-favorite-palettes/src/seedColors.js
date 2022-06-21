a02d73cfc29a41c0f3f49dce99da07c3b053b442
Johannes Beckmann <johannes-bec@t-online.de>
2022-06-21 23:55:19 +0200

    Initialize Code with first Seed palette

---
+++ my-favorite-palettes/src/seedColors.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    paletteName: "Botanic-Print",
+    id: "botanic",
+    emoji: "🎨",
+    colors: [
+      { name: "black", color: "#1C1C1A" },
+      { name: "green", color: "#8AD167" },
+      { name: "beige", color: "#DFDE9F" },
+      { name: "white", color: "#FFFFFF" },
+     
+    ]
+  },
+  
+];
