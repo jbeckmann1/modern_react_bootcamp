5fa63c3b1149a343ae016f56197ccbf0a8545992
Johannes Beckmann <johannes-bec@t-online.de>
2022-06-24 00:00:28 +0200

    NEW COLOR

--- my-favorite-palettes/src/seedColors.js
+++ my-favorite-palettes/src/seedColors.js
@@ -1,10 +1,55 @@
 export default [
   {
+    paletteName: "hellohello",
+    id: "hellohello",
+    emoji: "🎨",
+    colors: [
+      { name: "black", color: "#1C1C1A" },
+      { name: "green", color: "#8AD167" },
+      { name: "beige", color: "#DFDE9F" },
+      { name: "white", color: "#FFFFFF" },
+     
+    ]
+  }, {
+    paletteName: "Shakuro",
+    id: "Shakuro",
+    emoji: "🎨",
+    colors: [
+      { name: "black", color: "#145858" },
+      { name: "green", color: "#C8E0CA" },
+      { name: "beige", color: "#F7B318" },
+      { name: "white", color: "#FFFFFF" },
+     
+    ]
+  },
+  {
     paletteName: "Botanic-Print",
     id: "botanic",
     emoji: "🎨",
     colors: [
-      { name: "black", color: "#1C1C1A" },
+      { name: "black", color: "#151812" },
+      { name: "green", color: "#76CA71" },
+      { name: "beige", color: "#6148AA" },
+      { name: "white", color: "#FFFFFF" },
+     
+    ]
+  }, {
+    paletteName: "Botanic-Print",
+    id: "botanic",
+    emoji: "🎨",
+    colors: [
+      { name: "black", color: "#151515" },
+      { name: "green", color: "#85DD74" },
+      { name: "beige", color: "#E1F782" },
+      { name: "white", color: "#FFFFFF" },
+     
+    ]
+  }, {
+    paletteName: "Botanic-Print",
+    id: "botanic",
+    emoji: "🎨",
+    colors: [
+      { name: "black", color: "#" },
       { name: "green", color: "#8AD167" },
       { name: "beige", color: "#DFDE9F" },
       { name: "white", color: "#FFFFFF" },
